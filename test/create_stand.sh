c73400be631b268800f7864f306f1d95609796e0
Artem Zdor <a.zdor@smartblocklab.com>
2019-09-11 19:19:45 +0300

    add create_stand

---
+++ test/create_stand.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# https://github.com/UtahDave/salt-vagrant-demo
+
+git clone https://github.com/UtahDave/salt-vagrant-demo.git
+cd salt-vagrant-demo
+vagrant plugin install vagrant-vbguest
+vagrant up
+
+# vagrant ssh master
+# salt \* test.ping
